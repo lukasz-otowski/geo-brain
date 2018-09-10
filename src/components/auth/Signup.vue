fe212bc00e7982ef3897e61bc25756e8d3b99b32
Łukasz Otowski <lukaszotowski@interia.pl>
2018-09-10 18:02:56 +0200

    install and use slugify package

--- src/components/auth/Signup.vue
+++ src/components/auth/Signup.vue
@@ -14,6 +14,7 @@
             <label for="alias">Alias:</label>
             <input type="text" name="alias" v-model="alias" >
         </div>
+        <p class="red-text center" v-if="feedback">{{feedback}}</p>
         <div class="field center">
             <button class="btn deep-purple">Signup</button>
         </div>
@@ -22,18 +23,31 @@
 </template>
 
 <script>
+import slugify from 'slugify'    
+
 export default {
     name:'Signup',
     data(){
         return{
             email:null,
             password:null,
-            alias:null
+            alias:null,
+            feedback:null,
+            slug:null
         }
     },
     methods:{
         signup(){
-            
+            if(this.alias){
+                this.slug = slugify(this.alias, {
+                    replacement: '-',
+                    remove: /[$*_+~.()'"!\-:@]/g,
+                    lower:true
+                })
+                console.log(this.slug)
+            }else{
+                this.feedback = "You must enter an alias"
+            }
         }
     }
 }
