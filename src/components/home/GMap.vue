b298775298b1a53c60abfd72a7cb22e6a0416b6c
Łukasz Otowski <lukaszotowski@interia.pl>
2018-09-17 18:16:44 +0200

    route guarding

--- src/components/home/GMap.vue
+++ src/components/home/GMap.vue
@@ -5,63 +5,70 @@
 </template>
 
 <script>
-import firebase from 'firebase'
-import db from '@/firebase/init'
+    import firebase from 'firebase'
+    import db from '@/firebase/init'
 
-export default {
-    name:'GMap',
-    data() {
-        return{
-            lat:53,
-            lng:-2
-        }
-    },
-    methods: {
-        renderMap(){
-            const map = new google.maps.Map(document.getElementById('map'), {
-                center: {lat: this.lat, lng: this.lng},
-                zoom: 6,
-                maxZoom:15,
-                minZoom: 3,
-                streetViewControl:false
-            })
-        }
-    },
-    mounted(){
-        // get current user
-        let user = firebase.auth().currentUser
-        
-        //get user geolocation
-        if(navigator.geolocation){
-            navigator.geolocation.getCurrentPosition(pos =>{
-                this.lat = pos.coords.latitude
-                this.lng = pos.coords.longitude
-                
-                // find the user record and then update geocoords
-                db.collection('users').where('user_id', '==', user.uid).get()
-                .then(snapshot => {
-                    snapshot.forEach((doc) => {
-                        db.collection('users').doc(doc.id).update({
-                            geolocation: {
-                                lat: pos.coords.latitude,
-                                lng: pos.coords.longitude
-                            }
-                        })
-                    })
-                }).then(() => { 
-                    this.renderMap()
+    export default {
+        name: 'GMap',
+        data() {
+            return {
+                lat: 53,
+                lng: -2
+            }
+        },
+        methods: {
+            renderMap() {
+                const map = new google.maps.Map(document.getElementById('map'), {
+                    center: {
+                        lat: this.lat,
+                        lng: this.lng
+                    },
+                    zoom: 6,
+                    maxZoom: 15,
+                    minZoom: 3,
+                    streetViewControl: false
                 })
-                
-            }, (err) => {
-                console.log(err)
+            }
+        },
+        mounted() {
+            // get current user
+            let user = firebase.auth().currentUser
+
+            //get user geolocation
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(pos => {
+                    this.lat = pos.coords.latitude
+                    this.lng = pos.coords.longitude
+
+                    // find the user record and then update geocoords
+                    db.collection('users').where('user_id', '==', user.uid).get()
+                        .then(snapshot => {
+                            snapshot.forEach((doc) => {
+                                db.collection('users').doc(doc.id).update({
+                                    geolocation: {
+                                        lat: pos.coords.latitude,
+                                        lng: pos.coords.longitude
+                                    }
+                                })
+                            })
+                        }).then(() => {
+                            this.renderMap()
+                        })
+
+                }, (err) => {
+                    console.log(err)
+                    this.renderMap()
+                }, {
+                    maximumAge: 60000,
+                    timeout: 3000
+                })
+            } else {
+                //position centre by default values
                 this.renderMap()
-            }, {maximumAge: 60000, timeout: 3000})
-        } else {
-            //position centre by default values
-            this.renderMap()
-        }
-    },
-}
+            }
+        },
+    }
+
 </script>
 
 <style>
@@ -75,4 +82,5 @@ export default {
         left: 0;
         z-index: -1;
     }
+
 </style>
