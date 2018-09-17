01b048acd6cdb09885fd1ba33417f694451469ec
Łukasz Otowski <lukaszotowski@interia.pl>
2018-09-17 22:11:54 +0200

    add if condition for nav links

--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -2,11 +2,12 @@
     <div class="navbar">
         <nav class="deep-purple darken-1">
             <div class="container">
-                <router-link :to="{ name: 'GMap'}">Geo-Brain</router-link>
+                <router-link :to="{ name: 'GMap'}" class="brand-logo left" >Geo-Brain</router-link>
                 <ul class="right">
-                    <li><router-link :to="{name: 'Signup'}">Sign up</router-link></li>
-                    <li><router-link :to="{name: 'Login'}" >Login</router-link></li>
-                    <li><a @click="logout">Logout</a></li>
+                    <li v-if="!user"><router-link :to="{name: 'Signup'}">Sign up</router-link></li>
+                    <li v-if="!user"><router-link :to="{name: 'Login'}">Login</router-link></li>
+                    <li v-if="user" ><a>{{user.email}}</a></li>
+                    <li v-if="user"><a @click="logout">Logout</a></li>
                 </ul>
             </div>
         </nav>
@@ -19,7 +20,7 @@ export default {
     name:'Navbar',
     data(){
         return {
-            
+            user: null
         }
     },
     methods: {
@@ -28,6 +29,15 @@ export default {
                 this.$router.push({ name: 'Login'})
             })
         }
+    },
+    created(){
+        firebase.auth().onAuthStateChanged((user) => {
+            if(user){
+                this.user = user
+            } else {
+                this.user = null
+            }
+        })
     }
 }
 
